0c38154e089b1ffad0159bd9a85037c8a42b1807
KoyamaSohei <koyamaso0309@gmail.com>
2020-02-28 17:34:21 +0900

    drag & crop image

--- DFT_IDFT_App.cpp
+++ DFT_IDFT_App.cpp
@@ -5,9 +5,34 @@
 using namespace std;
 using namespace cv;
 
+const int minW = 200;
+const int minH = 20;
+
+Mat src;
+Mat cropped;
+Rect roi;
+int px, py, w, h;
+
 void help(char* progName) {
 	cout << "Usage:" << endl
 		 << progName << " [image_path]" << endl;
+}
+
+void mouseHandler(int event, int x, int y, int flags, void* param) {
+	switch (event) {
+	case CV_EVENT_MOUSEMOVE:
+		if (flags & CV_EVENT_FLAG_LBUTTON) {
+			w = min(max(x - px, minW),max(src.cols-px, minW));
+			h = min(max(y - py, minH),max(src.rows-py, minH));
+		}
+		break;
+	case CV_EVENT_LBUTTONDOWN:
+		px = x;
+		py = y;
+		w = minW;
+		h = minH;
+		break;
+	}
 }
 
 int main(int argc, char *argv[]) {
@@ -16,12 +41,29 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	const string filename = argv[1];
-	Mat src = imread(filename, IMREAD_GRAYSCALE);
+	src = imread(filename, IMREAD_GRAYSCALE);
 	if (src.empty()) {
 		cout << "Error when opening image: " << filename << endl;
 		return 1;
 	}
-	imshow("original image", src);
-	waitKey(0);
+	string origin_win = "original image";
+	imshow(origin_win, src);
+	px = 0;
+	py = 0;
+	w = src.cols;
+	h = src.rows;
+	cvSetMouseCallback(origin_win.c_str(), mouseHandler);
+	roi = Rect(px, py, w, h);
+	cropped = Mat(src, roi);
+	while (1) {
+		waitKey(10);
+		roi.x = px;
+		roi.y = py;
+		roi.width = w;
+		roi.height = h;
+		cout << roi << endl;
+		cropped = Mat(src, roi);
+		imshow("cropped", cropped);
+	}
 	return 0;
 }
