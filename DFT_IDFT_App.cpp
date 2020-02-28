5ed099d708cfd5f7bad8a06a5cfee5b7ce3e5315
KoyamaSohei <koyamaso0309@gmail.com>
2020-02-28 15:10:50 +0900

    input filename

--- DFT_IDFT_App.cpp
+++ DFT_IDFT_App.cpp
@@ -2,8 +2,19 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+void help(char* progName) {
+	cout << "Usage:" << endl
+		 << progName << " [image_path]" << endl;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		help(argv[0]);
+		return 1;
+	}
+	const string filename = argv[1];
+	cout << filename << endl;
 	return 0;
 }
